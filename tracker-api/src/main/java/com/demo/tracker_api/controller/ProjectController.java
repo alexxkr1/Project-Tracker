c5b11ff5695d0f276b73390ecc02acfe13957f9d
Alex Kren <alex.kren@flowit.ee>
2025-10-04 09:48:05 +0300

    Add swaggerdoc dependency and document project controller

--- tracker-api/src/main/java/com/demo/tracker_api/controller/ProjectController.java
+++ tracker-api/src/main/java/com/demo/tracker_api/controller/ProjectController.java
@@ -2,6 +2,9 @@ package com.demo.tracker_api.controller;
 
 import com.demo.tracker_api.entity.Project;
 import com.demo.tracker_api.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,19 +13,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
+@Tag(name = "Projects", description = "Operations for creating and retrieving projects.")
 public class ProjectController {
     private final ProjectService service;
     public ProjectController(ProjectService service) { this.service = service; }
 
+    @Operation(summary = "Get all projects")
     @GetMapping
     public List<Project> getAll() { return service.findAll(); }
 
+    @Operation(
+            summary = "Create a new project",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Project successfully created"),
+                    @ApiResponse(responseCode = "400", description = "Invalid project details")
+            }
+    )
     @PostMapping
     public ResponseEntity<Project> createProject(@RequestBody Project project) {
         Project saved = service.createProject(project);
         return ResponseEntity.created(URI.create("/api/projects" + saved.getId())).body(saved);
     }
 
+    @Operation(
+            summary = "Get a project by ID",
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Project successfully found"),
+                @ApiResponse(responseCode = "404", description = "Project not found")
+        }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Project> getProject(@PathVariable Long id) {
         return service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
